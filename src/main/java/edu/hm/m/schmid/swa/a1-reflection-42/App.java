4aedf2050ddb80ef67c1bace213f51509ced45e7
Michael Schmid <m.schmid@hm.edu>
2017-03-29 15:35:53 +0200

    project

---
+++ src/main/java/edu/hm/m/schmid/swa/a1-reflection-42/App.java
@@ -0,0 +1,13 @@
+package edu.hm.m.schmid.swa.a1-reflection-42;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
